fdcf26d4b76daffc246d1ba7f0d648e3bb6be853
xins Bin <q25a25q@live.com>
2015-11-12 21:44:53 +0800

    模板修改

--- Lyu.Utility/Lyu.Utility/Application/Services/Dto/Extensions/QueryResultOutputExtension.cs
+++ Lyu.Utility/Lyu.Utility/Application/Services/Dto/Extensions/QueryResultOutputExtension.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
 using AutoMapper.QueryableExtensions;
 using Lyu.Utility.Extensions;
 
@@ -6,13 +8,13 @@ namespace Lyu.Utility.Application.Services.Dto.Extensions
 {
     public static class QueryResultOutputExtension
     {
-        public static QueryResultOutput<TDto> ToOutputAsync<TDto>(this IQueryable query, QueryRequestInput option)
+        public static async Task<QueryResultOutput<TDto>> ToOutputAsync<TDto>(this IQueryable query, QueryRequestInput option)
         {
             var result = query.To<TDto>();
             var recordsTotal = result.Count();
             return new QueryResultOutput<TDto>()
             {
-                Data = result.Skip(option.Start).Take(option.Length).ToList(),
+                Data = await result.Skip(option.Start).Take(option.Length).ToListAsync(),
                 Draw = option.Draw,
                 RecordsTotal = recordsTotal,
                 RecordsFiltered = recordsTotal
