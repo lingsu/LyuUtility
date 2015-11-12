cc31a573edfb49246384e46b0de0bd66c22833bc
xins Bin <q25a25q@live.com>
2015-11-12 21:46:47 +0800

    添加RepositoryExtension

---
+++ Lyu.Utility/Lyu.Utility/EntityFramework/Extensions/RepositoryExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Abp.Domain.Repositories;
+
+namespace Lyu.Utility.EntityFramework.Extensions
+{
+    public static class RepositoryExtension
+    {
+        public static async Task DeleteAsync<TEntity>(this IRepository<TEntity> repository,int id) where TEntity : class, IEntity<int>
+        {
+            var entity = await repository.GetAsync(id);
+            await repository.DeleteAsync(entity);
+        }
+        public static async Task DeleteAsync<TEntity>(this IRepository<TEntity> repository, IEnumerable<int> id) where TEntity : class, IEntity<int>
+        {
+            foreach (var i in id)
+            {
+                await repository.DeleteAsync(i);
+            }
+        }
+        public static async Task DeleteAsync<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, IEnumerable<TPrimaryKey> id) where TEntity : class, IEntity<TPrimaryKey>
+        {
+            foreach (var i in id)
+            {
+                await repository.DeleteAsync(i);
+            }
+        }
+        public static async Task DeleteAsync<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, TPrimaryKey id) where TEntity : class, IEntity<TPrimaryKey>
+        {
+            var entity = await repository.GetAsync(id);
+            await repository.DeleteAsync(entity);
+        }
+    }
+}
