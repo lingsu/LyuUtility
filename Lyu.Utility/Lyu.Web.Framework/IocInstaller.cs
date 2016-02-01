eb5f74c9bfa97ab56d16dc5185361bf07a4c2d11
xins Bin <q25a25q@live.com>
2016-02-01 09:36:09 +0800

    添加 IocInstaller

---
+++ Lyu.Utility/Lyu.Web.Framework/IocInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Lyu.Web.Framework.UI;
+
+namespace Lyu.Web.Framework
+{
+    public class IocInstaller: IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IPageHeadBuilder>().ImplementedBy<PageHeadBuilder>().LifestylePerWebRequest());
+        }
+    }
+}
