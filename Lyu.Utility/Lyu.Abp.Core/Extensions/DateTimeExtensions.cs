d9e6d91d817ded76c77ef8775f0194edb07504c7
xins Bin <q25a25q@live.com>
2016-03-14 10:27:36 +0800

    添加日期扩展

---
+++ Lyu.Utility/Lyu.Abp.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lyu.Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// 获取时间相对唯一字符串
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="milsec">是否使用毫秒</param>
+        /// <returns></returns>
+        public static string ToUniqueString(this DateTime dateTime, bool milsec = false)
+        {
+            int sedonds = dateTime.Hour * 3600 + dateTime.Minute * 60 + dateTime.Second;
+            string value = string.Format("{0}{1}{2}", dateTime.ToString("yy"), dateTime.DayOfYear, sedonds);
+            return milsec ? value + dateTime.ToString("fff") : value;
+        }
+    }
+}
---
+++ Lyu.Utility/Lyu.Abp.Core/Extensions/DateTimeRangeExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Abp.Timing;
+
+namespace Lyu.Core.Extensions
+{
+    public static class DateTimeRangeExtensions
+    {
+        private static DateTime Now { get { return Clock.Now; } }
+        /// <summary>
+        /// 获取 上周的时间范围
+        /// </summary>
+        public static DateTimeRange LastWeek
+        {
+            get
+            {
+                DateTime now = Now;
+                DayOfWeek[] weeks =
+                {
+                    DayOfWeek.Sunday,
+                    DayOfWeek.Monday,
+                    DayOfWeek.Tuesday,
+                    DayOfWeek.Wednesday,
+                    DayOfWeek.Thursday,
+                    DayOfWeek.Friday,
+                    DayOfWeek.Saturday
+                };
+                int index = Array.IndexOf(weeks, now.DayOfWeek);
+                return new DateTimeRange(now.Date.AddDays(-index - 7), now.Date.AddDays(-index).AddMilliseconds(-1));
+            }
+        }
+
+        /// <summary>
+        /// 获取 本周的时间范围
+        /// </summary>
+        public static DateTimeRange ThisWeek
+        {
+            get
+            {
+                DateTime now = DateTime.Now;
+                DayOfWeek[] weeks =
+                {
+                    DayOfWeek.Sunday,
+                    DayOfWeek.Monday,
+                    DayOfWeek.Tuesday,
+                    DayOfWeek.Wednesday,
+                    DayOfWeek.Thursday,
+                    DayOfWeek.Friday,
+                    DayOfWeek.Saturday
+                };
+                int index = Array.IndexOf(weeks, now.DayOfWeek);
+                return new DateTimeRange(now.Date.AddDays(-index), now.Date.AddDays(7 - index).AddMilliseconds(-1));
+            }
+        }
+
+        /// <summary>
+        /// 获取 下周的时间范围
+        /// </summary>
+        public static DateTimeRange NextWeek
+        {
+            get
+            {
+                DateTime now = DateTime.Now;
+                DayOfWeek[] weeks =
+                {
+                    DayOfWeek.Sunday,
+                    DayOfWeek.Monday,
+                    DayOfWeek.Tuesday,
+                    DayOfWeek.Wednesday,
+                    DayOfWeek.Thursday,
+                    DayOfWeek.Friday,
+                    DayOfWeek.Saturday
+                };
+                int index = Array.IndexOf(weeks, now.DayOfWeek);
+                return new DateTimeRange(now.Date.AddDays(-index + 7), now.Date.AddDays(14 - index).AddMilliseconds(-1));
+            }
+        }
+    }
+}
